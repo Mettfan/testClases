aab73be7356496de9df4af7be21edb52c31896b7
Mettfan <cesaryannick6@gmail.com>
2022-07-24 15:24:34 -0500

    'modificacion'

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <div>
       <span>
-        Yannick
       </span>
       <img className = 'perrito' src = 'https://olondriz.com/wp-content/uploads/2020/04/ambar-perrito-1-1024x899.jpg'/>
       <h1>
